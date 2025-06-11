deb341411ef07120badaf14c4b2a290dced98d96
Raphael Barucho <134150030+RaphaelBarucho@users.noreply.github.com>
2025-06-11 12:26:11 -0300

    entendendo o valid

--- TechCompany-main/TechCompany-4/TechCompany-81dff6ed0ba432ce8f9dfbca9ea8db63c99a313c/new-tech-company/src/main/java/com/example/demo/security/SecurityPessoa.java
+++ TechCompany-main/TechCompany-4/TechCompany-81dff6ed0ba432ce8f9dfbca9ea8db63c99a313c/new-tech-company/src/main/java/com/example/demo/security/SecurityPessoa.java
@@ -1,5 +1,6 @@
 package com.example.demo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,11 +13,16 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class SecurityPessoa{
-
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		return http.csrf(csrf -> csrf.disable())
 				   .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				   .authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.GET , "/api/usuarios/login").hasRole("ADMIN")
+				   .authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.POST , "/api/usuarios/login").permitAll()
+				   .requestMatchers(HttpMethod.GET,"/").permitAll()
+				   .requestMatchers("/css/**",
+				   								"/js/**").permitAll()
+				   .requestMatchers(HttpMethod.GET, "/home").permitAll()
+				   .requestMatchers(HttpMethod.GET, "/api/usuarios").permitAll()
 				   .anyRequest().authenticated()
 				   ).addFilterBefore(new SecurityFilter(), UsernamePasswordAuthenticationFilter.class)
 				   .build();
