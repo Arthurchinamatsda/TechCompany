45667fb46b076211379ec1bc6c3fc9a09f4e0c44
Arthurchinamatsda <ademarteiyuchina@gmail.com>
2025-06-30 09:09:28 -0300

    javaScript
    d

---
+++ new-tech-company/src/main/java/com/example/demo/controller/ControllerHome.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerHome {
+    
+    @GetMapping("/")
+    public String index() {
+        return "IndexHome";  // vai para src/main/resources/templates/index.html
+    }
+    @GetMapping("/home")
+    public String home() {
+        return "IndexHomeHome";  // vai para src/main/resources/templates/index.html
+    }
+}
